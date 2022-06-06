3b964c9bf7b6f8639e24ca2ca27f463972ffacbf
Gilsonlgd <85958860+Gilsonlgd@users.noreply.github.com>
2022-06-06 15:35:14 -0300

    Add files via upload

---
+++ Árvore Genealógica Deuses Gregos.pl
@@ -0,0 +1,363 @@
+parent(caos, eros).
+parent(caos, tártaro).
+parent(caos, gaia).
+parent(caos, érebo).
+parent(caos, nix).
+parent(gaia, óreas).
+parent(gaia, urano).
+parent(gaia, ponto).
+parent(gaia, ciclopes).
+parent(gaia, hecatônquiros).
+parent(gaia, tifão).
+parent(gaia, equidna).
+parent(gaia, taumas).
+parent(gaia, fórcis).
+parent(gaia, ceto).
+parent(gaia, nereu).
+parent(gaia, euríbia).
+parent(urano, ciclopes).
+parent(urano, hecatônquiros).
+parent(tártaro, tifão).
+parent(tártaro, equidna).
+parent(ponto, taumas).
+parent(ponto, fórcis).
+parent(ponto, ceto).
+parent(ponto, nereu).
+parent(ponto, euríbia).
+parent(érebo, caronte).
+parent(nix, caronte).
+parent(tífão, órtros).
+parent(tífão, cérbero).
+parent(tífão, cólquida).
+parent(tífão, quimera).
+parent(tífão, hidra).
+parent(tífão, esfinge).
+parent(equidna, órtros).
+parent(equidna, cérbero).
+parent(equidna, cólquida).
+parent(equidna, quimera).
+parent(equidna, hidra).
+parent(equidna, esfinge).
+parent(gaia, réia).
+parent(gaia, mnemósine).
+parent(gaia, téia).
+parent(gaia, têmis).
+parent(gaia, febe).
+parent(gaia, tétis).
+parent(gaia, jápeto).
+parent(gaia, hipérion).
+parent(gaia, crio).
+parent(gaia, ceos).
+parent(gaia, oceano).
+parent(gaia, cronos).
+parent(urano, réia).
+parent(urano, mnemósine).
+parent(urano, téia).
+parent(urano, têmis).
+parent(urano, febe).
+parent(urano, tétis).
+parent(urano, jápeto).
+parent(urano, hipérion).
+parent(urano, crio).
+parent(urano, ceos).
+parent(urano, oceano).
+parent(urano, cronos).
+parent(oceano, dóris).
+parent(oceano, tique).
+parent(oceano, ásia).
+parent(oceano, pleione).
+parent(oceano, estige).
+parent(oceano, métis).
+parent(oceano, eurínome).
+parent(tétis, dóris).
+parent(tétis, tique).
+parent(tétis, ásia).
+parent(tétis, pleione).
+parent(tétis, estige).
+parent(tétis, métis).
+parent(tétis, eurínome).
+parent(jápeto, átlas).
+parent(jápeto, prometeu).
+parent(jápeto, epimeteu).
+parent(ásia, átlas).
+parent(ásia, prometeu).
+parent(ásia, epimeteu).
+parent(epimeteu, pandora).
+parent(hipérion, hélio).
+parent(hipérion, selene).
+parent(téia, hélio).
+parent(téia, selene).
+parent(crio, palas).
+parent(euríbia, palas).
+parent(cronos, poseidon).
+parent(cronos, deméter).
+parent(cronos, zeus).
+parent(cronos, hera).
+parent(cronos, hades).
+parent(cronos, héstia).
+parent(réia, poseidon).
+parent(réia, deméter).
+parent(réia, zeus).
+parent(réia, hera).
+parent(réia, hades).
+parent(réia, héstia).
+parent(nereu, anfitrite).
+parent(nereu, tétis_).
+parent(dóris, anfitrite).
+parent(dóris, tétis_).
+parent(átlas, maia).
+parent(pleione, maia).
+parent(ceos, leto).
+parent(ceos, astéria).
+parent(febe, leto).
+parent(febe, astéria).
+parent(estige, nice).
+parent(estige, zelos).
+parent(estige, kratos).
+parent(estige, bia).
+parent(palas, nice).
+parent(palas, zelos).
+parent(palas, kratos).
+parent(palas, bia).
+parent(poseidon, tritão).
+parent(anfitrite, tritão).
+parent(métis, atena).
+parent(zeus, atena).
+parent(zeus, hebe).
+parent(zeus, hefesto).
+parent(zeus, ares).
+parent(hera, hebe).
+parent(hera, hefesto).
+parent(hera, ares).
+parent(zeus, afrodite).
+parent(afrodite, adônis).
+parent(zeus, hermes).
+parent(maia, hermes).
+parent(zeus, apolo).
+parent(leto, apolo).
+parent(zeus, ártemis).
+parent(leto, ártemis).
+parent(peleu, aquiles).
+parent(tétis_, aquiles).
+parent(poseidon, teseu).
+parent(poseidon, belerofonte).
+parent(zeus, perseu).
+parent(zeus, hércules).
+parent(zeus, helena).
+parent(zeus, minos).
+parent(zeus, dionísio).
+parent(mulher_mortal, perseu).
+parent(mulher_mortal, hércules).
+parent(mulher_mortal, helena).
+parent(mulher_mortal, minos).
+parent(mulher_mortal, dionísio).
+parent(zeus, cárites).
+parent(eurínome, cárites).
+parent(zeus, diké).
+parent(têmis, diké).
+parent(zeus, irene).
+parent(têmis, irene).
+parent(zeus, eunomia).
+parent(têmis, eunomia).
+parent(zeus, horas).
+parent(têmis, horas).
+parent(afrodite, eros).
+parent(hermes, hermafrodite).
+parent(afrodite, hermafrodite).
+
+god(caos, vazio).
+god(eros, amor).
+god(tártaro, personificação_do_mundo_inferior).
+god(gaia, terra).
+god(érebo, escuridão).
+god(nix, noite).
+god(urano, céu).
+god(ponto, mar).
+god(euríbia, fúria).
+god(tique, sorte).
+god(métis, saúde).
+god(selene, lua).
+god(poseidon, mares_e_rios).
+god(deméter, agricultura).
+god(zeus, trovão_e_céus_e_raio).
+god(hera, fertilidade_e_casamento).
+god(hades, mortos).
+god(héstia, fogo_sagrado).
+god(leto, anoitecer).
+god(astéria, estrelas).
+god(nice, vitória).
+god(zelo, rivalidade).
+god(astéria, estrelas).
+god(atena, sabedoria).
+god(hebe, juventude).
+god(hefesto, fogo).
+god(ares, guerra).
+god(afrodite, amor).
+god(hermes, velocidade_e_comércio).
+god(apolo, sol).
+god(ártemis, caça).
+god(dionísio, vinho).
+god(cárites, graças).
+god(eurínome, deusa_criadora).
+god(diké, justiça).
+god(eunomia, legalidade).
+god(irene, paz).
+god(eros, amor).
+god(hermafrodite, androgenia).
+god(chronos, tempo).
+
+nameis(tifão, gigante).
+nameis(ciclopes, ciclopes).
+nameis(hecatônquiros, hecatônquiros).
+nameis(equidna, monsto).
+nameis(taumas, titã).
+nameis(fórcis, titã).
+nameis(ceto, titã).
+nameis(nereu, velho_do_mar).
+nameis(caronte, barqueiro_do_submundo).
+nameis(órtros, cão_bicéfalo).
+nameis(cérbero, cão_de_três_cabeças).
+nameis(nereu, velho_do_mar).
+nameis(cólquida, dragão).
+nameis(quimera, criatura).
+nameis(hidra, monstro).
+nameis(esfinge, criatura).
+nameis(réia, titã).
+nameis(mnemósine, titã).
+nameis(téia, titã).
+nameis(têmis, titã).
+nameis(febe, titã).
+nameis(tétis, titã).
+nameis(jápeto, titã).
+nameis(hipérion, titã).
+nameis(crio, titã).
+nameis(ceos, titã).
+nameis(oceano, titã).
+nameis(cronos, titã).
+nameis(átlas, titã).
+nameis(prometeu, titã).
+nameis(epimeteu, titã).
+nameis(kratos, titã).
+nameis(bia, titã).
+nameis(kratos, titã).
+nameis(dóris, oceânides).
+nameis(ásia, oceânides).
+nameis(pleione, oceânides).
+nameis(eurínome, oceânides).
+nameis(estige, ninfa).
+nameis(tétis_, ninfa).
+nameis(maia, plêiades).
+nameis(pandora, mulher_mortal).
+nameis(adônis, homem_mortal).
+nameis(aquiles, herói).
+nameis(teseu, herói).
+nameis(belerofonte, herói).
+nameis(perseu, herói).
+nameis(hércules, herói).
+nameis(minos, semideus).
+
+nome(tritão, rei_dos_mares).
+nome(helena, mulher_mais_bela_do_mundo).
+nome(palas, filha_de_tritão).
+nome(hélio, personificação_do_sol).
+nome(anfitrite, rainha_dos_oceanos).
+
+olimpois(zeus).
+olimpois(hera).
+olimpois(poseidon).
+olimpois(atena).
+olimpois(ares).
+olimpois(deméter). 
+olimpois(apolo).
+olimpois(átermis).
+olimpois(hefeso).
+olimpois(afrodite).
+olimpois(hermes).
+olimpois(dionísio).
+
+horais(diké).
+horais(eunomia).
+horais(irene).
+
+primordialis(caos).
+primordialis(gaia).
+primordialis(tártaro).
+primordialis(eros).
+primordialis(nix).
+primordialis(érebo).
+primordialis(éter).
+primordialis(hemera).
+primordialis(ponto).
+primordialis(urano).
+primordialis(óreas).
+
+tem_filhos_com(X,Y) :-
+parent(X,F), parent(Y,F), Y \== X.
+
+
+filho_de(X,Y) :-
+	parent(X, Y).
+
+filhos_de(X, Filhos) :-
+	setof(Y, parent(X,Y), Filhos),
+	!.
+
+filhos_de(X, Filhos) :-
+	not(setof(Y, parent(X,Y), Filhos)),		
+	Filhos = unknown.	
+
+pai_de(X, Y) :-
+    parent(Y, X).
+
+pais_de(X, Pais) :-
+	setof(Y, parent(Y, X), Pais),
+	!.
+
+pais_de(X, Pais) :-
+	not(setof(Y, parent(Y, X), Pais)),		
+	Pais = unknown.
+
+irmaos_a(X, Y) :- %vem duplicado
+	parent(Z, X),
+	parent(Z, Y),
+	X \= Y.
+
+lista_de_irmaos(X, Irmaos) :-
+	setof(Y, irmaos_a(X,Y), Irmaos);	%Lista que exclui duplicados 
+	Irmaos = none.
+
+irmaos(X, Y) :- % funciona do jeito certo kkk
+	lista_de_irmaos(X, Irmaos),
+	member(Y, Irmaos).
+
+ancestral(X, Y) :-
+    parent(X, Y).
+
+ancestral(X, Y) :-
+    parent(X, Z),
+    ancestral(Z, Y), 
+    Y \== X.
+
+ancestrais(X, Ancestral_de) :-
+    findall(A, ancestral(X, A), Ancestral_de).
+
+sobre(X) :-
+    god(X, Y),
+	format("~w é o(a) deus(a) do(a) ~w", [X, Y]), nl.
+
+sobre(X) :-
+	nameis(X, Y),
+	format("~w é um/uma ~w", [X, Y]), nl.
+
+sobre(X) :-
+	nome(X, Y),
+	format("~w é o/a ~w", [X, Y]), nl.
+
+info(X) :-
+	pais_de(X, Pais),
+	format("Pais: ~w", [Pais]), nl, 
+	filhos_de(X, Filhos),
+	format("Filhos: ~w", [Filhos]), nl,
+	lista_de_irmaos(X, Irmaos),
+	format("Irmaos: ~w", [Irmaos]), nl,
+	!.
